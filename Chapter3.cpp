aab10812bac3f1dee3e05fa6243c8fdee7fc1a8f
Shoaib-CS <shoaibcs252@gmail.com>
2024-07-03 02:32:11 +0500

    Changes in chapter 3

--- Chapter3.cpp
+++ Chapter3.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void header();//function decleration to print header
-void calculatePayableAmount(char day, double totalAmount);//function decleration to calculate discount
+void header();                                             // function decleration to print header
+void calculatePayableAmount(char day, double totalAmount); // function decleration to calculate discount
 main()
 {
 
-   header(); //Calling function
+    header(); // Calling function
 
-   char day;
-   double totalAmount;
-   cout << "Enter the day (S for Sunday and O for other): ";
-   cin >> day;
-   cout << "Enter the total purchase amount: ";
-   cin >> totalAmount;
-   calculatePayableAmount(day, totalAmount);//Calling function
+    char day;
+    double totalAmount;
+    cout << "Enter the day (S for Sunday and O for other): ";
+    cin >> day;
+    cout << "Enter the total purchase amount: ";
+    cin >> totalAmount;
+    calculatePayableAmount(day, totalAmount); // Calling function
 }
 void header()
 {
-    //function to print header
+    // function to print header
     cout << "--------------------------------------------------------------------------------------------------------------------------------------" << endl;
     cout << "|   ######    #####    ######  ###  ##   ######            #####   #######            #####    #####   ####     #######   #####      |" << endl;
     cout << "|    ##  ##  ##   ##     ##    #### ##     ##             ##   ##   ##  ##           ##   ##  ##   ##   ##       ##  ##  ##   ##     |" << endl;
@@ -36,21 +36,20 @@ void header()
     cout << "|             ##   ##   ##       ##       ##  ##     ##    ##   ##  ##   ##     ##       ##    ##   ##  ##   ##                      |" << endl;
     cout << "|             ##   ##  ####     ####      ######   ######   #####   ##   ##     ##     ######   #####   ##   ##                      |" << endl;
     cout << "--------------------------------------------------------------------------------------------------------------------------------------" << endl;
-
 }
 
 void calculatePayableAmount(char day, double totalAmount)
 {
-    //function to find discount
+    // function to find discount
 
-   if (day == 'S') //condition to check if it is sunday
-   {
-      double discount = totalAmount * 0.1;
-      double payableAmount = totalAmount - discount;
-      cout << "Payable amount after 10% discount: " << payableAmount;
-   }
-   if (day == 'O') //condition to check if it is other day
-   {
-      cout << "Payable amount: " << totalAmount;
-   }
+    if (day == 'S') // condition to check if it is sunday
+    {
+        double discount = totalAmount * 0.1;
+        totalAmount = totalAmount - discount;
+        cout << "Payable amount after 10% discount: " << totalAmount;
+    }
+    if (day == 'O') // condition to check if it is other day
+    {
+        cout << "Payable amount without discount: " << totalAmount;
+    }
 }
